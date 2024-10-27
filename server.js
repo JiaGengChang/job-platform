a034dc450498b0d02a5fb86625bcfb06901f561f
thinkpad-t460s <jiageng1997@gmail.com>
2024-10-28 00:24:40 +0800

    route to index page

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); //allow return of json data
-
-app.get("/", (req,res)=>{res.send('Index page goes here');});
+app.use(express.static(__dirname + '/public')); //serve static files
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 app.use("/api/v1/jobs", jobRoutes); // CRUD for job postings
 app.use("/api/v1/users", userRoutes); // CRUD for user accounts
 app.use("/api/v1/auth", authRoutes); // login, register, sign-out
